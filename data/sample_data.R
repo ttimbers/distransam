3712ab7626005d8bf71b10860bef8fdb2572aca7
ttimbers <tiffany.timbers@gmail.com>
2016-04-25 16:33:01 -0700

    added outline of tests to write

---
+++ data/sample_data.R
@@ -0,0 +1,4 @@
+# sample data
+strain <- c(rep("N2", 5), rep("CB1", 7))
+plate <- c(rep('a', 2), rep('b', 3), rep('a', 4), rep('b',3))
+x <- data.frame(strain, plate)
---
+++ tests/testthat/test-distransam.R
@@ -0,0 +1,22 @@
+context("distransam")
+library(gapminder)
+
+test_that("randomly_sampled_dataframe is the same width and types as input data frame", {
+})
+
+test_that("if sample_var = NULL, N for each group should be equal in randomly_sampled_dataframe",{
+  
+})
+
+test_that("if sample_var = !NULL, N for each group should be equal in randomly_sampled_dataframe", {
+  
+})
+
+test_that("if sample_var = NULL, then N must be > minimum # individuals in smallest group", {
+  
+})
+  
+test_that("if sample_var = !NULL, then N/# of samples must be > minimum # individuals in 
+  smallest sample within all groups", {
+    
+  })
