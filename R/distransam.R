a0c783afb04ab3075159287a206df3d46bc2c51a
ttimbers <tiffany.timbers@gmail.com>
2016-04-25 16:22:45 -0700

    working draft of the distransam function

--- R/distransam.R
+++ R/distransam.R
@@ -1,14 +1,33 @@
-distransam ->  function(dataframe, grouping_var, sample_var = NULL, N){
+library(dplyr)
+
+distransam <- function(x, grouping_var, sample_var = NULL, N){
   
   # case where we want to randomly sample only across groups (no plates/sites to worry about)
-  if (is.null(sample_var) {
-    # grab N random sample from unique factors in grouping_var column
+  if (is.null(sample_var)) {
+  # grab N random sample from unique factors in grouping_var column
     
-    # case where we want to randomly sample only across groups, and we want to spread these
-    # equally across the sample variable (e.g., plates)
-  } else {
-    # grab N random sample from unique combinations of factors in grouping_var & sample_var column
+    # get the minimum sample size of each group
+    x_counts <- group_by_(x, grouping_var) %>% summarise(n())
+    min_sample <- min(x_counts$`n()`)
+    
+    # sample the minimum number of samples from each group
+    randomly_sampled_dataframe <- data.frame(group_by_(x, grouping_var) %>% sample_n(size = min_sample))
+                   
+  # case where we want to randomly sample only across groups, and we want to spread these
+  # equally across the sample variable (e.g., plates)
+  } else{
+  # grab N random sample from unique combinations of factors in grouping_var & sample_var column
+    
+    # get the minimum sample size of each group
+    x_counts <- x %>% group_by_(grouping_var, sample_var) %>% summarise(n())
+    min_sample <- min(x_counts$`n()`)
+    
+    # sample the minimum number of samples from each group
+    randomly_sampled_dataframe <- data.frame(x %>% group_by_(grouping_var, sample_var) %>% sample_n(size = min_sample))
+    
   }
   
   return(randomly_sampled_dataframe)
 }
+  
+  
