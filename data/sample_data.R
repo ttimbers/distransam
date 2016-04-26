722e5af9611c1b95408c578db226e0702ac38b58
ttimbers <tiffany.timbers@gmail.com>
2016-04-26 16:08:23 -0700

    updated code for sample data

--- data/sample_data.R
+++ data/sample_data.R
@@ -1,4 +1,3 @@
 # sample data
-strain <- c(rep("N2", 5), rep("CB1", 7))
-plate <- c(rep('a', 2), rep('b', 3), rep('a', 4), rep('b',3))
-x <- data.frame(strain, plate)
+worm_data <- data.frame(c(rep("N2", 5), rep("CB1", 7)), c(rep('a', 2), rep('b', 3), rep('a', 4), rep('b',3)), sample(5:10, 12, replace = TRUE))
+colnames(worm_data) <- c('strain', 'plate', 'measurement')
