533ca31216eeab1aa40ee631e91943bb0ac6884a
ttimbers <tiffany.timbers@gmail.com>
2016-08-11 12:23:17 -0700

    Fixed tests so that they work as expected for time series data, as well as non-time series data

--- tests/testthat/test-distransam.R
+++ tests/testthat/test-distransam.R
@@ -1,5 +1,5 @@
 context("distransam")
-library(dply)
+library(dplyr)
 library(purrr)
 
 # Determine if range of vector is FP 0. (borrowed from Hadley Wickam - http://stackoverflow.com/questions/4752275/test-for-equality-among-all-elements-of-a-single-vector)
@@ -10,10 +10,10 @@ zero_range <- function(x, tol = .Machine$double.eps ^ 0.5) {
 }
 
 # create test data
-group <- c(rep('Drug1', 12), rep('Drug2', 17))
-site <- c(rep('Site1', 5), rep('Site2', 4), rep('Site3', 3), rep('Site4', 4), rep('Site5', 4), rep('Site6', 5), rep('Site7', 4))
-sample <- c(seq(1:5), seq(1:4), seq(1:3), seq(1:4), seq(1:4), seq(1:5), seq(1:4))
-measurement <- c(rnorm(12, mean = 5, sd = 0.5), rnorm(17, mean = 6.5, sd = 0.5))
+group <- c(rep('Drug1', 28), rep('Drug2', 46))
+site <- c(rep('Site1', 10), rep('Site2', 12), rep('Site3', 6), rep('Site4', 12), rep('Site5', 12), rep('Site6', 10), rep('Site7', 12))
+sample <- c(rep(seq(1:5), 2), rep(seq(1:4), 3), rep(seq(1:3), 2), rep(seq(1:4), 3), rep(seq(1:4), 3), rep(seq(1:5), 2), rep(seq(1:4), 3))
+measurement <- c(rnorm(28, mean = 5, sd = 0.5), rnorm(46, mean = 6.5, sd = 0.5))
 test_data <- data.frame(group, site, sample, measurement)
 
 # run distransam function on test data
@@ -30,7 +30,8 @@ test_that("N for each group should be equal in randomly_sampled_dataframe",{
 })
 
 test_that("N for each site should be equal in randomly_sampled_dataframe", {
-  zero_range(data_counts_evenly_sampled$site$freq)
+  evenly_sampled_sample_counts <- unlist(evenly_sampled_data %>% split(.$site)  %>% map(~ length(unique(.$sample))))
+  zero_range(evenly_sampled_sample_counts)
 })
 
 test_that("N sites per group must be equal to the minimum number of sites in the smallest group", {
@@ -40,6 +41,7 @@ test_that("N sites per group must be equal to the minimum number of sites in the
 })
 
 test_that("N for samples per site must be equal to the minimum number of samples in the smallest site", {
-  test_counts <- test_data %>% group_by(group) %>% map(plyr::count)
-  all(min(test_counts$site$freq) ==  data_counts_evenly_sampled$site$freq)
+  test_data_sample_counts <- unlist(test_data %>% split(.$site)  %>% map(~ length(unique(.$sample))))
+  evenly_sampled_sample_counts <- unlist(evenly_sampled_data %>% split(.$site)  %>% map(~ length(unique(.$sample))))
+  all(min(test_data_sample_counts) == evenly_sampled_sample_counts)
 })
