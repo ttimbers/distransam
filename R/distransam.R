f81bd1f8fa509981a81c1be844bb06565242f040
ttimbers <tiffany.timbers@gmail.com>
2016-10-22 12:21:09 -0700

    Got distramsam function workinggit add .git add .git add .

--- R/distransam.R
+++ R/distransam.R
@@ -20,14 +20,57 @@
 #' work in progress
 distransam <- function(x, grouping_var, site_var, sample_var){
 
-  # query how many sites per group, and if not equal, randomly select them from each group to make them equal to
-  # the number of sites in the smallest group
-
-
-
-  # query how many samples per site, and if not equal, randomly select them from each site to make them equal to
-  # the number of samples in the smallest site
-
+  # make table with counts from dataset (strain_n, min_n_id_per_plate,  min_n_plates_per_strain)
+  # id counts
+  id_counts <- x %>%
+    group_by(group) %>%
+    count(plate, id) %>% 
+    group_by(group, plate) %>% 
+    summarise(count = n())
+  
+  # number of strains
+  strain_n <- id_counts %>% 
+    select(group) 
+  strain_n <- nrow(unique(strain_n))
+  
+  # minimum number of samples per plate
+  min_n_id_per_plate <- id_counts %>% 
+    ungroup() %>% 
+    select(count) %>% 
+    min()
+  
+  # minimum number of plates per strain
+  min_n_plates_per_strain <- id_counts %>% 
+    group_by(group) %>%
+    summarise(count = n()) %>% 
+    select(count) %>% 
+    min()
+  
+  count_summary <- data.frame(stat = c("Number of Strains", 
+               "Min N of samples per plate", 
+               "Min N of plates per strain"), 
+             value = c(strain_n, min_n_id_per_plate, min_n_plates_per_strain))
+  
+  # randomly select min_n_plates from each strain
+  randomly_sampled_dataframe <- x %>% 
+    group_by(group, plate, id) %>% 
+    nest(.key = id_data) %>% 
+    ungroup() %>% 
+    group_by(group, plate) %>% 
+    nest(.key = plate_data) %>% 
+    ungroup() %>% 
+    group_by(group) %>% 
+    nest(.key = strain_data) %>%  
+    mutate(samp = map2(strain_data, min_n_plates_per_strain, sample_n)) %>% 
+    select(group, samp) %>% 
+    unnest() %>% 
+    # randomly select min_n_id_per_plate from each plate
+    mutate(samp = map2(plate_data, min_n_id_per_plate, sample_n)) %>% 
+    select(group, plate, samp) %>% 
+    unnest() %>% 
+    unnest()
+    
+  # return data frame
   return(randomly_sampled_dataframe)
 }
 
