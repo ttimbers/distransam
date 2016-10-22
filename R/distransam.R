f30fab545ca8cf075116c91d4440747e0dedd091
ttimbers <tiffany.timbers@gmail.com>
2016-10-22 13:41:35 -0700

    Made function flexible for col names

--- R/distransam.R
+++ R/distransam.R
@@ -10,7 +10,7 @@
 #'
 #' @param x dataframe
 #' @param grouping_var string
-#' @param site_var string
+#' @param plate_var string
 #' @param sample_var string
 #'
 #' @return dataframe
@@ -18,59 +18,59 @@
 #'
 #' @examples
 #' work in progress
-distransam <- function(x, grouping_var, site_var, sample_var){
+distransam <- function(x, grouping_var, plate_var, sample_var){
 
   # make table with counts from dataset (strain_n, min_n_id_per_plate,  min_n_plates_per_strain)
   # id counts
   id_counts <- x %>%
-    group_by(group) %>%
-    count(plate, id) %>% 
-    group_by(group, plate) %>% 
+    group_by_(grouping_var) %>%
+    count_(c(plate_var, sample_var)) %>%
+    group_by_(grouping_var, plate_var) %>%
     summarise(count = n())
-  
+
   # number of strains
-  strain_n <- id_counts %>% 
-    select(group) 
+  strain_n <- id_counts %>%
+    select_(grouping_var)
   strain_n <- nrow(unique(strain_n))
-  
+
   # minimum number of samples per plate
-  min_n_id_per_plate <- id_counts %>% 
-    ungroup() %>% 
-    select(count) %>% 
+  min_n_id_per_plate <- id_counts %>%
+    ungroup() %>%
+    select(count) %>%
     min()
-  
+
   # minimum number of plates per strain
-  min_n_plates_per_strain <- id_counts %>% 
-    group_by(group) %>%
-    summarise(count = n()) %>% 
-    select(count) %>% 
+  min_n_plates_per_strain <- id_counts %>%
+    group_by_(grouping_var) %>%
+    summarise(count = n()) %>%
+    select(count) %>%
     min()
-  
-  count_summary <- data.frame(stat = c("Number of Strains", 
-               "Min N of samples per plate", 
-               "Min N of plates per strain"), 
+
+  count_summary <- data.frame(stat = c("Number of Strains",
+               "Min N of samples per plate",
+               "Min N of plates per strain"),
              value = c(strain_n, min_n_id_per_plate, min_n_plates_per_strain))
   print(count_summary)
-  
+
   # randomly select min_n_plates from each strain
-  randomly_sampled_dataframe <- x %>% 
-    group_by(group, plate, id) %>% 
-    nest(.key = id_data) %>% 
-    ungroup() %>% 
-    group_by(group, plate) %>% 
-    nest(.key = plate_data) %>% 
-    ungroup() %>% 
-    group_by(group) %>% 
-    nest(.key = strain_data) %>%  
-    mutate(samp = map2(strain_data, min_n_plates_per_strain, sample_n)) %>% 
-    select(group, samp) %>% 
-    unnest() %>% 
+  randomly_sampled_dataframe <- x %>%
+    group_by_(grouping_var, plate_var, sample_var) %>%
+    nest(.key = id_data) %>%
+    ungroup() %>%
+    group_by_(grouping_var, plate_var) %>%
+    nest(.key = plate_data) %>%
+    ungroup() %>%
+    group_by_(grouping_var) %>%
+    nest(.key = strain_data) %>%
+    mutate(samp = map2(strain_data, min_n_plates_per_strain, sample_n)) %>%
+    select_(grouping_var, "samp") %>%
+    unnest() %>%
     # randomly select min_n_id_per_plate from each plate
-    mutate(samp = map2(plate_data, min_n_id_per_plate, sample_n)) %>% 
-    select(group, plate, samp) %>% 
-    unnest() %>% 
+    mutate(samp = map2(plate_data, min_n_id_per_plate, sample_n)) %>%
+    select_(grouping_var, plate_var, "samp") %>%
+    unnest() %>%
     unnest()
-    
+
   # return data frame
   return(randomly_sampled_dataframe)
 }
--- tests/testthat/test-distransam.R
+++ tests/testthat/test-distransam.R
@@ -3,82 +3,82 @@ library(dplyr)
 library(purrr)
 
 # create test data
-group <- c(rep('Strain1', 28), 
+group <- c(rep('Strain1', 28),
            rep('Strain2', 46))
 
-plate <- c(rep('Plate1', 10), 
-           rep('Plate2', 12), 
-           rep('Plate3', 6), 
-           rep('Plate4', 12), 
-           rep('Plate5', 12), 
-           rep('Plate6', 10), 
+plate <- c(rep('Plate1', 10),
+           rep('Plate2', 12),
+           rep('Plate3', 6),
+           rep('Plate4', 12),
+           rep('Plate5', 12),
+           rep('Plate6', 10),
            rep('Plate7', 12))
 
-id <- c(rep(1, 5), rep(2, 5), 
+id <- c(rep(1, 5), rep(2, 5),
         rep(1, 4), rep(2, 4), rep(3, 4),
-        rep(1, 3), rep(2, 3), 
+        rep(1, 3), rep(2, 3),
         rep(1, 4), rep(2, 4), rep(3, 4),
-        rep(1, 4), rep(2, 4), rep(3, 4), 
-        rep(1, 5), rep(2, 5), 
+        rep(1, 4), rep(2, 4), rep(3, 4),
+        rep(1, 5), rep(2, 5),
         rep(1, 4), rep(2, 4), rep(3, 4))
 
-time <- c(rep(seq(1:5), 2), 
-          rep(seq(1:4), 3), 
-          rep(seq(1:3), 2), 
-          rep(seq(1:4), 3), 
-          rep(seq(1:4), 3), 
-          rep(seq(1:5), 2), 
+time <- c(rep(seq(1:5), 2),
+          rep(seq(1:4), 3),
+          rep(seq(1:3), 2),
+          rep(seq(1:4), 3),
+          rep(seq(1:4), 3),
+          rep(seq(1:5), 2),
           rep(seq(1:4), 3))
 
-measurement <- c(rnorm(28, mean = 5, sd = 0.5), 
+measurement <- c(rnorm(28, mean = 5, sd = 0.5),
                  rnorm(46, mean = 6.5, sd = 0.5))
 
 test_data <- data.frame(group, plate, id, time, measurement)
 test_data
 
 # create expected correct answer from test data
-group <- c(rep('Strain1', 24), 
+group <- c(rep('Strain1', 24),
            rep('Strain2', 26))
 
-plate <- c(rep('Plate1', 10), 
-           rep('Plate2', 8), 
-           rep('Plate3', 6), 
-           rep('Plate4', 8), 
-           #rep('Plate5', 12), 
-           rep('Plate6', 10), 
+plate <- c(rep('Plate1', 10),
+           rep('Plate2', 8),
+           rep('Plate3', 6),
+           rep('Plate4', 8),
+           #rep('Plate5', 12),
+           rep('Plate6', 10),
            rep('Plate7', 8))
 
-id <- c(rep(1, 5), rep(2, 5), 
+id <- c(rep(1, 5), rep(2, 5),
         rep(1, 4), rep(2, 4), #rep(3, 4),
-        rep(1, 3), rep(2, 3), 
+        rep(1, 3), rep(2, 3),
         rep(1, 4), rep(2, 4), #rep(3, 4),
-        #rep(1, 4), rep(2, 4), rep(3, 4), 
-        rep(1, 5), rep(2, 5), 
+        #rep(1, 4), rep(2, 4), rep(3, 4),
+        rep(1, 5), rep(2, 5),
         rep(1, 4), rep(2, 4)) #rep(3, 4))
 
-time <- c(rep(seq(1:5), 2), 
-          rep(seq(1:4), 2), 
-          rep(seq(1:3), 2), 
-          rep(seq(1:4), 2), 
-          #rep(seq(1:4), 3), 
-          rep(seq(1:5), 2), 
+time <- c(rep(seq(1:5), 2),
+          rep(seq(1:4), 2),
+          rep(seq(1:3), 2),
+          rep(seq(1:4), 2),
+          #rep(seq(1:4), 3),
+          rep(seq(1:5), 2),
           rep(seq(1:4), 2))
 
-measurement <- c(rnorm(24, mean = 5, sd = 0.5), 
+measurement <- c(rnorm(24, mean = 5, sd = 0.5),
                  rnorm(26, mean = 6.5, sd = 0.5))
 
 exp_data <- data.frame(group, plate, id, time, measurement)
 exp_data
 
 # run distransam function on test data
-evenly_sampled_data <- distransam(test_data)
+evenly_sampled_data <- distransam(test_data, "group", "plate", "id")
 
 # calculate id counts for use in tests
 # counts for expected data
 data_counts_exp <- exp_data %>%
 group_by(group) %>%
-  count(plate, id) %>% 
-  group_by(group, plate) %>% 
+  count(plate, id) %>%
+  group_by(group, plate) %>%
   summarise(count = n())
 # counts for test data set generated via distransam
 data_counts_evenly_sampled <- evenly_sampled_data %>%
@@ -102,7 +102,7 @@ plate_counts_evenly_sampled <- data_counts_evenly_sampled %>%
 test_that("randomly_sampled_dataframe is the same width and types as input data frame", {
   # test expected data set (generated manually)
   expect_equal(dim(test_data)[2], dim(exp_data)[2])
-  
+
   # test data set generated via distransam
   expect_equal(dim(test_data)[2], dim(evenly_sampled_data)[2])
 })
@@ -110,7 +110,7 @@ test_that("randomly_sampled_dataframe is the same width and types as input data 
 test_that("N (id's) for each plate should be equal in randomly_sampled_dataframe",{
   # test expected data set (generated manually)
   zero_range(data_counts_exp$count)
-  
+
   # test data set generated via distransam
   zero_range(data_counts_evenly_sampled$count)
 })
@@ -118,7 +118,7 @@ test_that("N (id's) for each plate should be equal in randomly_sampled_dataframe
 test_that("N for each Plate should be equal in randomly_sampled_dataframe", {
   # test expected data set (generated manually)
   zero_range(plate_counts_exp$count)
-  
+
   # test data set generated via distransam
   zero_range(plate_counts_evenly_sampled$count)
 })
@@ -127,17 +127,17 @@ test_that("N Plates per group must be equal to the minimum number of Plates in t
   # get min plate per group/strain
   min_plate_test_data <- test_data %>%
     group_by(group) %>%
-    count(plate, id) %>% 
-    group_by(group, plate) %>% 
-    summarise(count = n()) %>% 
+    count(plate, id) %>%
+    group_by(group, plate) %>%
+    summarise(count = n()) %>%
     group_by(group) %>%
-    summarise(count = n()) %>% 
-    select(count) %>% 
+    summarise(count = n()) %>%
+    select(count) %>%
     min()
-  
+
   # test expected data set (generated manually)
   all(min_plate_test_data == plate_counts_exp$count)
-  
+
   # test data set generated via distransam
   all(min_plate_test_data == plate_counts_evenly_sampled$count)
 })
@@ -146,16 +146,16 @@ test_that("N for samples per Plate must be equal to the minimum number of sample
   # get min id per plate
   min_ids_test_data <- test_data %>%
     group_by(group) %>%
-    count(plate, id) %>% 
-    group_by(group, plate) %>% 
-    summarise(count = n()) %>% 
-    ungroup() %>% 
-    select(count) %>% 
+    count(plate, id) %>%
+    group_by(group, plate) %>%
+    summarise(count = n()) %>%
+    ungroup() %>%
+    select(count) %>%
     min()
-  
+
   # test expected data set (generated manually)
   all(min_ids_test_data == data_counts_exp$count)
-  
+
   # test data set generated via distransam
   all(min_ids_test_data == plate_counts_evenly_sampled$count)
 })
