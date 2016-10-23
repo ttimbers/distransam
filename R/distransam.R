a09c3415a2baa7521123e5f547b731dd15559e2c
ttimbers <tiffany.timbers@gmail.com>
2016-10-22 21:10:18 -0700

    Improved documentation to match function

--- R/distransam.R
+++ R/distransam.R
@@ -1,12 +1,15 @@
-#' Randomly samples groups to get equal N's for both sites and samples from each group
+#' Randomly samples a time series groups and plates/sites to get equal N's for both
+#' plates/sites and samples from each group
 #'
-#' Takes a data frame, and returns one with equal N's for both sites and samples from each group. For
-#' unequal site numbers, it finds the minimum site number from the smallest group, and randomly
+#' Takes a data frame containing a time series, a column name for the grouping variable,
+#' a column name for the plates/sites variable, and a column name for the sample id's.
+#' Returns one with equal N's for both plates/sites and samples from each group.
+#'
+#' For unequal site numbers, it finds the minimum site number from the smallest group, and randomly
 #' selects that number of sites from each group. For unequal sample sizes between sites, it finds the
 #' minimum sample size from the smallest site, and randomly selects that number of samples from each
-#' group. The returned object is a dataframe, with all the same original columns as the original data
-#' frame. This function also works for time series data (where you have mutliple measurements from
-#' different time points for each sample).
+#' group. The returned object is a dataframe, with all the same columns as the original data
+#' frame.
 #'
 #' @param x dataframe
 #' @param grouping_var string
@@ -14,10 +17,11 @@
 #' @param sample_var string
 #'
 #' @return dataframe
-#' @export
 #'
 #' @examples
-#' work in progress
+#'
+#' # distransam(data_frame, "strain", "plate", "id")
+#'
 distransam <- function(x, grouping_var, plate_var, sample_var){
 
   # make table with counts from dataset (strain_n, min_n_id_per_plate,  min_n_plates_per_strain)
